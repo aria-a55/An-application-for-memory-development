5f00b639b6ee877c8d2b3c20e10d80e73a6a3d62
4414_Паксялина Дарья Сергеевна <paksyalinnd@mail.ru>
2025-12-09 22:22:25 +0300

    changing the logic of the memorization game

--- memory_game_logic.py
+++ memory_game_logic.py
@@ -1,5 +1,6 @@
 import random
 
+
 class MemoryGame:
     def __init__(self, words_list, max_rounds=10, max_errors=3):
         self.WORDS = words_list
@@ -13,18 +14,32 @@ class MemoryGame:
         self.score = 0
         self.errors = 0
         self.game_over = False
+        # Список уже использованных слов для текущей последовательности
+        self.used_words = []
 
     def generate_sequence(self):
-        self.sequence = []
-        for _ in range(self.current_round):
+        # Для первого раунда - одно случайное слово
+        if self.current_round == 1:
             word = random.choice(self.WORDS)
-            while self.sequence and word == self.sequence[-1]:
-                word = random.choice(self.WORDS)
-            self.sequence.append(word)
-        return self.sequence
+            self.sequence = [word]
+            self.used_words = [word]
+        else:
+            # Для последующих раундов сохраняем предыдущую последовательность
+            # и добавляем новое слово
+            available_words = [w for w in self.WORDS if w not in self.used_words]
+
+            # Если все слова использованы, очищаем список использованных
+            if not available_words:
+                available_words = self.WORDS.copy()
+                self.used_words = []
+
+            new_word = random.choice(available_words)
+            self.sequence.append(new_word)
+            self.used_words.append(new_word)
+
+        return self.sequence.copy()
 
     def check_answer(self, user_words):
-
         user_words_list = [w.strip() for w in user_words.split()]
 
         if len(user_words_list) != len(self.sequence):
@@ -38,12 +53,12 @@ class MemoryGame:
         return True
 
     def process_correct_answer(self):
-        self.score += self.current_round * 10
+        # Начисляем очки: 10 за каждое слово в последовательности
+        self.score += len(self.sequence) * 10
         self.current_round += 1
 
         # Проверяем, не закончилась ли игра по количеству раундов
         if self.current_round > self.max_rounds:
-
             self.game_over = True
 
         return True
@@ -66,5 +81,10 @@ class MemoryGame:
             'errors': self.errors,
             'max_rounds': self.max_rounds,
             'max_errors': self.max_errors,
-            'sequence': self.sequence.copy()
+            'sequence': self.sequence.copy(),
+            'sequence_length': len(self.sequence)
         }
+
+    def get_display_sequence(self):
+        """Возвращает последовательность для отображения пользователю"""
+        return " ".join(self.sequence)
